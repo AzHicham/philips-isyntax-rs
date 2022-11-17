9e33418f9ea16b274362041fac2383996d4bfef6
Hicham <hicham.azimani@owki.com>
2022-11-17 08:47:02 +0100

    fix: build doc on docs.rs

--- build.rs
+++ build.rs
@@ -1,18 +1,22 @@
+use std::env;
+
 fn main() -> miette::Result<()> {
-    cxx_build::bridge("src/bindings.rs")
-        .file("cpp/philipsslide.cc")
-        .flag_if_supported("-std=c++17")
-        .compile("philips-bindings");
+    if env::var("DOCS_RS").is_err() {
+        cxx_build::bridge("src/bindings.rs")
+            .file("cpp/philipsslide.cc")
+            .flag_if_supported("-std=c++17")
+            .compile("philips-bindings");
 
-    println!("cargo:rerun-if-changed=src/bindings.rs");
-    println!("cargo:rerun-if-changed=cpp/philipsslide.cc");
-    println!("cargo:rerun-if-changed=cpp/philipsslide.hpp");
+        println!("cargo:rerun-if-changed=src/bindings.rs");
+        println!("cargo:rerun-if-changed=cpp/philipsslide.cc");
+        println!("cargo:rerun-if-changed=cpp/philipsslide.hpp");
 
-    println!("cargo:rustc-link-lib=pixelengine");
-    println!("cargo:rustc-link-lib=softwarerendercontext");
-    println!("cargo:rustc-link-lib=eglrendercontext");
-    println!("cargo:rustc-link-lib=softwarerenderbackend");
-    println!("cargo:rustc-link-lib=gles2renderbackend");
-    println!("cargo:rustc-link-lib=gles3renderbackend");
+        println!("cargo:rustc-link-lib=pixelengine");
+        println!("cargo:rustc-link-lib=softwarerendercontext");
+        println!("cargo:rustc-link-lib=eglrendercontext");
+        println!("cargo:rustc-link-lib=softwarerenderbackend");
+        println!("cargo:rustc-link-lib=gles2renderbackend");
+        println!("cargo:rustc-link-lib=gles3renderbackend");
+    }
     Ok(())
 }
